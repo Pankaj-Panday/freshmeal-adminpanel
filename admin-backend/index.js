9b6984b2054d00232bcdfad5510924891cecfdae
Pankaj Panday <ppanday064@gmail.com>
2026-06-15 14:20:08 +0530

    refactor: move global request logging middleware to the top of the middleware stack

--- admin-backend/index.js
+++ admin-backend/index.js
@@ -71,6 +71,11 @@ app.post(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  console.log("REQUEST", req.method, req.originalUrl);
+  next();
+});
+
 app.use(cors());
 app.use(helmet());
 
@@ -93,11 +98,6 @@ app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/orders", orderRoutes);
 app.use("/api/v1/users", userRoutes);
-
-app.use((req, res, next) => {
-  console.log("REQUEST", req.method, req.originalUrl);
-  next();
-});
 
 app.use(errorHandler);
 
